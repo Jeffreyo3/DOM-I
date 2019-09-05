525a8540f8f8219feedf82a94ffb5b8571c11d29
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2019-09-04 19:51:04 -0700

    completed adding missing text from the index.html using DOM selectors

--- js/index.js
+++ js/index.js
@@ -37,6 +37,70 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
+// Example: Update the img src for the logo //
+
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Nav
+
+const nav = document.querySelectorAll('nav a');
+nav[0].textContent = 'Services';
+nav[1].textContent = 'Product';
+nav[2].textContent = 'Vision';
+nav[3].textContent = 'Features';
+nav[4].textContent = 'About';
+nav[5].textContent = 'Contact';
+
+
+// CTA //
+
+const ctaText = document.querySelector('.cta-text h1');
+ctaText.textContent = 'DOM Is Awesome';
+
+const ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent = 'Get Started';
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = 'img/header-img.png';
+
+
+// Main Content //
+
+const mainTextH = document.querySelectorAll('.text-content h4');
+mainTextH[0].textContent = 'Features';
+mainTextH[1].textContent = 'About';
+mainTextH[2].textContent = 'Services';
+mainTextH[3].textContent = 'Product';
+mainTextH[4].textContent = 'Vision';
+
+const mainTextP = document.querySelectorAll('.text-content p');
+// Top Content
+mainTextP[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+mainTextP[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+// Middle Content
+const middleImg = document.querySelector('#middle-img');
+middleImg.src = 'img/mid-page-accent.jpg';
+
+// Bottom Content
+mainTextP[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+mainTextP[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+mainTextP[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+
+// Contact //
+
+const contactH = document.querySelector('.contact h4');
+contactH.textContent = 'Contact';
+
+const contactP = document.querySelectorAll('.contact p');
+contactP[0].textContent = '123 Way 456 Street Somewhere, USA';
+contactP[1].textContent = '1 (888) 888-8888';
+contactP[2].textContent = 'sales@greatidea.io';
+
+
+// Footer //
+
+const footP = document.querySelector('footer p');
+footP.textContent = 'Copyright Great Idea! 2018';
